b872460127cf8f0bc1e075fa531e6efe6315d13d
shawonNirob <shawonnirob16@gmail.com>
2024-11-10 23:11:09 +0600

    Composition Advantage

---
+++ src/main/java/oops/MockitoTestObject.java
@@ -0,0 +1,27 @@
+package oops;
+
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+
+class Engine1{
+    public void start(){
+        System.out.println("Start");
+    }
+}
+
+class car{
+    private Engine1 engine;
+
+    public car(Engine1 engine){
+        engine = new Engine1();
+    }
+    public void start(){
+        engine.start();
+    }
+}
+
+public class MockitoTestObject {
+    public static void main(String[] args) {
+
+    }
+}
